aea6f587e007facffab90267e837309a623678c6
Guillermo-NH <ingnanco@gmail.com>
2023-06-05 22:57:34 -0600

    mover el scroll al final

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -2,14 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final ScrollController chatScrollController = ScrollController();
+
   List<Message> messagesList = [
     Message(text: 'Hola amor!', fromWho: FromWho.hers),
     Message(text: 'Ya regresaste del trabajo?', fromWho: FromWho.me),
   ];
 
   Future<void> sendMessage(String text) async {
+    if (text.isEmpty) return;
     final newMessage = Message(text: text, fromWho: FromWho.me);
     messagesList.add(newMessage);
     notifyListeners();
+    moveScrollBotton();
+  }
+
+  Future<void> moveScrollBotton() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+    chatScrollController.animateTo(
+        chatScrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.bounceIn);
   }
 }
